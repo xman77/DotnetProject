96f82c3ce549d1eed816facaff5a29eeffebfe9e
xman77 <xman77@gmail.com>
2019-02-09 01:43:15 +0800

    Initial commit  Changes to be committed: 	new file:   .vs/spider/DesignTimeBuild/.dtbcache 	new file:   .vs/spider/v15/.suo 	new file:   .vs/spider/v15/Server/sqlite3/db.lock 	new file:   .vs/spider/v15/Server/sqlite3/storage.ide 	new file:   .vs/spider/v15/Server/sqlite3/storage.ide-shm 	new file:   .vs/spider/v15/Server/sqlite3/storage.ide-wal 	new file:   spider.sln 	new file:   spider/Program.cs 	new file:   spider/bin/Debug/netcoreapp2.0/chromedriver.exe 	new file:   spider/bin/Debug/netcoreapp2.0/debug.log 	new file:   spider/bin/Debug/netcoreapp2.0/spider.deps.json 	new file:   spider/bin/Debug/netcoreapp2.0/spider.dll 	new file:   spider/bin/Debug/netcoreapp2.0/spider.pdb 	new file:   spider/bin/Debug/netcoreapp2.0/spider.runtimeconfig.dev.json 	new file:   spider/bin/Debug/netcoreapp2.0/spider.runtimeconfig.json 	new file:   spider/chromedriver.exe 	new file:   spider/obj/Debug/netcoreapp2.0/spider.AssemblyInfo.cs 	new file:   spider/obj/Debug/netcoreapp2.0/spider.AssemblyInfoInputs.cache 	new file:   spider/obj/Debug/netcoreapp2.0/spider.csproj.CoreCompileInputs.cache 	new file:   spider/obj/Debug/netcoreapp2.0/spider.csproj.FileListAbsolute.txt 	new file:   spider/obj/Debug/netcoreapp2.0/spider.csprojAssemblyReference.cache 	new file:   spider/obj/Debug/netcoreapp2.0/spider.dll 	new file:   spider/obj/Debug/netcoreapp2.0/spider.pdb 	new file:   spider/obj/project.assets.json 	new file:   spider/obj/spider.csproj.nuget.cache 	new file:   spider/obj/spider.csproj.nuget.g.props 	new file:   spider/obj/spider.csproj.nuget.g.targets 	new file:   spider/spider.csproj 	new file:   spider/spider.csproj.user

---
+++ spider/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace spider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var driver = new ChromeDriver("./");
+            //driver.Url = "http://www.google.com";
+            //driver.Manage().Window.Maximize();
+            //Thread.Sleep(2000);
+            //IWebElement inputAccount = driver.FindElementByXPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input");
+            //inputAccount.Clear();
+            //Thread.Sleep(2000);
+            //inputAccount.SendKeys("selenium");
+            //inputAccount.Submit();
+            //Thread.Sleep(2000);
+            //driver.Quit();
+            var driver = new ChromeDriver("./");
+            //開啟網頁
+            driver.Navigate().GoToUrl("https://louislinebot.azurewebsites.net/Login");
+            //隱式等待 - 直到畫面跑出資料才往下執行
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10000);
+
+            //輸入帳號
+            IWebElement inputAccount = driver.FindElement(By.Name("Account"));
+            Thread.Sleep(2000);
+            //清除按鈕
+            inputAccount.Clear();
+            Thread.Sleep(2000);
+            inputAccount.SendKeys("20180513");
+            Thread.Sleep(2000);
+
+            //輸入密碼
+            IWebElement inputPassword = driver.FindElement(By.Name("Passwrod"));
+
+            inputPassword.Clear();
+            Thread.Sleep(2000);
+            inputPassword.SendKeys("123456");
+            Thread.Sleep(2000);
+
+            //點擊執行
+            IWebElement submitButton = driver.FindElement(By.XPath("/html/body/div[2]/form/table/tbody/tr[4]/td[2]/input"));
+            Thread.Sleep(2000);
+            submitButton.Click();
+            Thread.Sleep(2000);
+
+            driver.Quit();
+        }
+    }
+}
